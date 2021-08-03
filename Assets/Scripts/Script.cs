153290209b5fec0f66898dca26897921d4fda9d7
playstation7 <neket.1992@mail.ru>
2021-08-04 02:39:21 +0300

    commit

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scritp : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    
+    }
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
