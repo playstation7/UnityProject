df591203d5a516fd7ba5aeb68eeac81809bf51f3
Timur Salekhov <saltimure@gmail.com>
2021-08-04 13:57:44 +0300

    background created

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        movement.x = 0.1f;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
