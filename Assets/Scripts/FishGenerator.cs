9307d2754531d4ab42a4cc8c83670f6f73ed5001
Timur Salekhov <saltimure@gmail.com>
2021-08-04 15:17:08 +0300

    gitignore modified

--- Assets/Scripts/FishGenerator.cs
+++ Assets/Scripts/FishGenerator.cs
@@ -9,7 +9,7 @@ public class FishGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        createFish();
+        StartCoroutine(createFish());
     }
 
     // Update is called once per frame
@@ -18,8 +18,11 @@ public class FishGenerator : MonoBehaviour
         
     }
 
-    private void createFish()
+    private IEnumerator createFish()
     {
+
+        yield return new WaitForSeconds(Random.Range(1f, 4f));
+
         GameObject fish = Instantiate(fishPrefab);
 
         bool rightFish = Random.Range(0,2) == 1;
@@ -31,6 +34,7 @@ public class FishGenerator : MonoBehaviour
         {
             x = 11;
             fish.GetComponent<Fish>().movement.x = -0.4f;
+            fish.GetComponent<Transform>().Rotate(0f, 180f, 0f);
         } else
         {
             x = -11;
@@ -38,5 +42,7 @@ public class FishGenerator : MonoBehaviour
         }
 
         fish.GetComponent<Transform>().position = new Vector3(x,y,1);
+
+        StartCoroutine(createFish());
     }
 }
